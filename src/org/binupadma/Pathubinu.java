639b2574175e36e7ddff1f1ad13a2f24fdcdf315
binu21 <verginpreethibinu21@gmail.com>
2018-12-11 22:00:05 +0530

    padmabai

--- src/org/binupadma/Pathubinu.java
+++ src/org/binupadma/Pathubinu.java
@@ -4,5 +4,6 @@ public class Pathubinu {
 public static void main(String[] args) {
 	System.out.println("padmabinu");
 	System.out.println("binu and padmavathibai");
+	System.out.println("padmabai");
 }
 }
